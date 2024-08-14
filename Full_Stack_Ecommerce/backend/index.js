4132e0544fe31f25eeb81feff8a3f378093235b2
Tonish <122773804+Tonish01@users.noreply.github.com>
2024-08-14 21:06:11 +0530

    Update index.js

--- Full_Stack_Ecommerce/backend/index.js
+++ Full_Stack_Ecommerce/backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection With MongoDB
-mongoose.connect("mongodb+srv://Ecommerce:Ecommerce@ecommerce.fnigj.mongodb.net/e-commerce");
+mongoose.connect("your mongodb connection link/e-commerce");
 
 // paste your mongoDB Connection string above with password
 // password should not contain '@' special character
